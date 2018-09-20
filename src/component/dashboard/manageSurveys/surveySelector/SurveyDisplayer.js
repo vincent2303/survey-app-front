940d7973ac128a382d3499010b4fbc80f2358546
vincent2303 <vincent.martin23@gmail.com>
2018-09-20 17:28:53 +0200

    surveyDislayer

--- src/component/dashboard/manageSurveys/surveySelector/SurveyDisplayer.js
+++ src/component/dashboard/manageSurveys/surveySelector/SurveyDisplayer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ThematiqueDisplayer from './thematiqueDisplayer';
+import {GridList, GridListTile} from '@material-ui/core';
 
 const SurveyDisplayer = ({sondage}) => {
     return (
-        <div>
+        <GridList spacing={20} cellHeight={'auto'} cols={3} style={{marginTop:'2vh', marginBottom:'6vh'}}>
             {sondage.thematiqueList.map(thematique=>(
-                <ThematiqueDisplayer key={thematique.id} thematique={thematique}/>
+                <GridListTile key={thematique.id+'_gridTile'}>
+                    <ThematiqueDisplayer key={thematique.id} thematique={thematique}/>
+                </GridListTile>
             ))}
-        </div>
+        </GridList>
     )
 }
 
--- src/component/dashboard/manageSurveys/surveySelector/thematiqueDisplayer.js
+++ src/component/dashboard/manageSurveys/surveySelector/thematiqueDisplayer.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Card, Typography } from '@material-ui/core';
 
+const titleStyle = { fontFamily: 'Roboto', fontSize: '1.5em', color: '#2c3e50', fontWeight: 500, textAlign:'center'}
+const questionStyle = { fontFamily: 'Roboto', fontSize: '1em', color: '#2c3e50', fontWeight: 100, marginBottom:'1vh'}
 
 const ThematiqueDisplayer = ({thematique}) => {
     return (
-        <div>
-            <h3>
-            {thematique.name}
-            </h3>
-                {thematique.questionList.map((question)=>(
-                    <p key={question.id} >{question.question}</p>
-                ))}
-        </div>
+        <Card style={{backgroundColor: '#ecf0f1', margin: '1vh', padding: '1vh'}} >
+            <Typography style={titleStyle} >
+                {thematique.name}
+            </Typography>
+            {thematique.questionList.map((question)=>(
+                <Typography key={question.id} style={questionStyle} >{question.question}</Typography>
+            ))}
+        </Card>
     )
 }
 

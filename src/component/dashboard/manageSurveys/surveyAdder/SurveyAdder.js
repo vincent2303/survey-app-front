d6aa63c55f364da9ae37a99ffbb0352fe25b4e7e
vincent2303 <vincent.martin23@gmail.com>
2018-09-20 10:50:08 +0200

    ff

--- src/component/dashboard/manageSurveys/surveyAdder/SurveyAdder.js
+++ src/component/dashboard/manageSurveys/surveyAdder/SurveyAdder.js
@@ -3,9 +3,11 @@ import { Paper, Typography } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import idGenerator from '../../../../customFunction/idGenerator'
+import ThematiqueAdder from './ThematiqueAdder';
 
 
 const titleStyle = { fontFamily: 'Roboto', fontSize: '2.5em', color: '#2c3e50', fontWeight: 100, textAlign:'center'}
+
 
 class SurveyAdder extends Component {
 
@@ -15,10 +17,17 @@ class SurveyAdder extends Component {
     }
 
     addThematique = ()=>{
-        const idgen = idGenerator()
+        let emptyThematique = {
+            name: '',
+            questionMap: new Map()
+        }
         let newThematiqueMap = this.state.thematiqueMap
-        newThematiqueMap.set(idgen, idgen)
+        newThematiqueMap.set(idGenerator(), emptyThematique)
         this.setState({thematiqueMap: newThematiqueMap})
+    }
+
+    addQuestion = ()=>{
+        console.log(this.state)
     }
 
     render(){
@@ -33,13 +42,19 @@ class SurveyAdder extends Component {
                     margin="normal"
                 />
                 <br/>
+                {Array.from(this.state.thematiqueMap.keys()).map(key=>(
+                    <ThematiqueAdder 
+                        key={key} 
+                        ThematiqueId={key} 
+                        thematique={this.state.thematiqueMap.get(key)}
+                         
+                    />
+                ))}
+                <br/>
                 <Button variant="contained" onClick={this.addThematique} >
                     Add a thematique
                 </Button>
-                <br/>
-                {Array.from(this.state.thematiqueMap.keys()).map(key=>(
-                    <p key={key} >valeur : {key}</p>
-                ))}
+
             </Paper>
         )
     }
---
+++ src/component/dashboard/manageSurveys/surveyAdder/ThematiqueAdder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ThematiqueAdder = ({ThematiqueId, thematique, addQuestion}) => {
+    console.log(ThematiqueId)
+    return (
+    <p>ddd</p>
+)}
+
+
+export default ThematiqueAdder
